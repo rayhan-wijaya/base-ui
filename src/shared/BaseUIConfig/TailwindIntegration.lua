8709268058cfe35fe6196f42bd6adee5724b0934
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-06-01 00:07:08 +0700

    Created example TailwindIntegration config

---
+++ src/shared/BaseUIConfig/TailwindIntegration.lua
@@ -0,0 +1,11 @@
+type StyleMappings = {[string]: GuiBase}
+
+local customStyleMappings: StyleMappings = {
+  -- Custom style mappings!!
+  -- e.g.
+  -- ["bg-transparent"] = { BackgroundTransparency = 0 },
+}
+
+return {
+  customStyleMappings = customStyleMappings,
+}
