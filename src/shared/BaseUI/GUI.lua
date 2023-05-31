b60b990bcd50783bdff029180cccbe44a2bc0a60
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-05-31 21:19:59 +0700

    Optionally use Styles module at GUI.lua

--- src/shared/BaseUI/GUI.lua
+++ src/shared/BaseUI/GUI.lua
@@ -1,6 +1,12 @@
 --!strict
 
 local core = require(script.Parent.Core)
+local applyStyles = nil
+
+if script.Parent.OptionalModules:FindFirstChild("Styles") then
+  applyStyles = require(script.Parent.OptionalModules.Styles.ApplyStyles)
+end
+
 local types = require(script.Parent.Types)
 
 local gui = {
@@ -10,6 +16,7 @@ local gui = {
     core.assignPropsToGuiObject(props, newScreenGui)
     core.assignChildrenToParent(children, newScreenGui)
     core.assignGuiObjectToRef(newScreenGui, props.Ref)
+    if applyStyles then applyStyles.applyStylesToGuiObject(props.Styles, newScreenGui) end
 
     return newScreenGui
   end,
@@ -19,6 +26,7 @@ local gui = {
     core.assignPropsToGuiObject(props, newFrame)
     core.assignChildrenToParent(children, newFrame)
     core.assignGuiObjectToRef(newFrame, props.Ref)
+    if applyStyles then applyStyles.applyStylesToGuiObject(props.Styles, newFrame) end
 
     return newFrame
   end,
@@ -28,6 +36,7 @@ local gui = {
     core.assignPropsToGuiObject(props, newTextButton)
     newTextButton.Text = children[1];
     core.assignGuiObjectToRef(newTextButton, props.Ref)
+    if applyStyles then applyStyles.applyStylesToGuiObject(props.Styles, newTextButton) end
 
     return newTextButton
   end,
@@ -37,6 +46,7 @@ local gui = {
     core.assignPropsToGuiObject(props, newTextLabel)
     newTextLabel.Text = children[1];
     core.assignGuiObjectToRef(newTextLabel, props.Ref)
+    if applyStyles then applyStyles.applyStylesToGuiObject(props.Styles, newTextLabel) end
 
     return newTextLabel
   end,
@@ -46,6 +56,7 @@ local gui = {
     core.assignPropsToGuiObject(props, newImageLabel)
     newImageLabel.Image = children[1];
     core.assignGuiObjectToRef(newImageLabel, props.Ref)
+    if applyStyles then applyStyles.applyStylesToGuiObject(props.Styles, newImageLabel) end
 
     return newImageLabel
   end,
