8c9892f7f0d470964ea1486ebb33c559986b8f74
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-05-31 21:18:19 +0700

    Created bg-transparent style

--- src/shared/BaseUI/OptionalModules/Styles/StyleMappings.lua
+++ src/shared/BaseUI/OptionalModules/Styles/StyleMappings.lua
@@ -1,4 +1,7 @@
 local styleMappings: {[string]: GuiObject} = {
+  -- BG
+  ["bg-transparent"] = { BackgroundTransparency = 0 },
+
   -- BG Red
   ["bg-red-100"] = { BackgroundColor3 = Color3.fromRGB(255, 223, 223) },
   ["bg-red-500"] = { BackgroundColor3 = Color3.fromRGB(255, 78, 78) },
