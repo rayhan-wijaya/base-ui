ea34fe7a194fa0f50d160c2a5bf9b0604bf972e4
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-05-31 23:13:18 +0700

    Created bg-slate styles

--- src/shared/BaseUI/OptionalModules/TailwindIntegration/StyleMappings.lua
+++ src/shared/BaseUI/OptionalModules/TailwindIntegration/StyleMappings.lua
@@ -2,10 +2,18 @@ local styleMappings: {[string]: GuiBase} = {
   -- BG
   ["bg-transparent"] = { BackgroundTransparency = 0 },
 
-  -- BG Red
-  ["bg-red-100"] = { BackgroundColor3 = Color3.fromRGB(255, 223, 223) },
-  ["bg-red-500"] = { BackgroundColor3 = Color3.fromRGB(255, 78, 78) },
-  ["bg-red-900"] = { BackgroundColor3 = Color3.fromRGB(66, 20, 20) },
+  -- BG Slate
+  ["bg-slate-50"] = { BackgroundColor3 = Color3.fromHex("#f8fafc") },
+  ["bg-slate-100"] = { BackgroundColor3 = Color3.fromHex("#f1f5f9") },
+  ["bg-slate-200"] = { BackgroundColor3 = Color3.fromHex("#e2e8f0") },
+  ["bg-slate-300"] = { BackgroundColor3 = Color3.fromHex("#cbd5e1") },
+  ["bg-slate-400"] = { BackgroundColor3 = Color3.fromHex("#94a3b8") },
+  ["bg-slate-500"] = { BackgroundColor3 = Color3.fromHex("#64748b") },
+  ["bg-slate-600"] = { BackgroundColor3 = Color3.fromHex("#475569") },
+  ["bg-slate-700"] = { BackgroundColor3 = Color3.fromHex("#334155") },
+  ["bg-slate-800"] = { BackgroundColor3 = Color3.fromHex("#1e293b") },
+  ["bg-slate-900"] = { BackgroundColor3 = Color3.fromHex("#0f172a") },
+  ["bg-slate-950"] = { BackgroundColor3 = Color3.fromHex("#020617") },
 }
 
 return styleMappings
