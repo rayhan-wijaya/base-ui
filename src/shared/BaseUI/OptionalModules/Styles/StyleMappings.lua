103764e4bdfbf1ebb21299616795dc22cc0e1883
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-05-31 21:18:05 +0700

    Created StyleMappings

---
+++ src/shared/BaseUI/OptionalModules/Styles/StyleMappings.lua
@@ -0,0 +1,8 @@
+local styleMappings: {[string]: GuiObject} = {
+  -- BG Red
+  ["bg-red-100"] = { BackgroundColor3 = Color3.fromRGB(255, 223, 223) },
+  ["bg-red-500"] = { BackgroundColor3 = Color3.fromRGB(255, 78, 78) },
+  ["bg-red-900"] = { BackgroundColor3 = Color3.fromRGB(66, 20, 20) },
+}
+
+return styleMappings
