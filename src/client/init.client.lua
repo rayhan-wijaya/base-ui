f863e595c89f52642bbab8a8f88dc8aeb0658c69
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-05-31 12:23:31 +0700

    Created example usage at init.client.lua

--- src/client/init.client.lua
+++ src/client/init.client.lua
@@ -1,1 +1,48 @@
-print("Hello world, from client!")
+local player = game.Players.LocalPlayer
+local playerGui = player.PlayerGui
+
+local baseUI = game.ReplicatedStorage.Common.BaseUI
+local gui = require(baseUI.GUI)
+
+local frame = function ()
+  local refs  = {
+    firstButton = nil :: TextButton?,
+  }
+
+  local frame = (
+    gui.frame({
+      Size = UDim2.new(1, 0, 1, 0),
+      BackgroundTransparency = 1,
+    }, {
+      gui.textButton({
+        BackgroundColor3 = Color3.fromRGB(255, 255, 255),
+        Ref = { refKey = "firstButton", refs = refs },
+      }, {
+        "0"
+      }),
+      gui.textButton({ BackgroundColor3 = Color3.fromRGB() }, {
+        "Second Button",
+      }),
+    })
+  )
+
+  local updateFirstButton = coroutine.create(function ()
+    while task.wait(0.5) do
+      local nextNumber = tonumber(refs.firstButton.Text) + 1
+      refs.firstButton.Text = tostring(nextNumber)
+    end
+  end)
+
+  coroutine.resume(updateFirstButton)
+
+  return frame
+end
+
+local screenGui = (
+  gui.screenGui({}, {
+    frame(),
+  })
+)
+
+screenGui.Parent = playerGui
+
