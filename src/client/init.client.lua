bace7b77bcdaa2543e3b40c582298eece555c8a4
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-06-01 00:30:02 +0700

    Two-spaces instead of tabs

--- src/client/init.client.lua
+++ src/client/init.client.lua
@@ -5,38 +5,38 @@ local player = game.Players.LocalPlayer
 local playerGui = player.PlayerGui
 
 local frame = function ()
-	local refs = {
-		textLabel = nil :: TextLabel?,
-	}
+  local refs = {
+    textLabel = nil :: TextLabel?,
+  }
 
   local newFrame = (
-		gui.frame({ Styles = "bg-red-900 text-scaled" }, {
-			gui.textLabel({
-				Styles = "bg-red-300 text-scaled",
-				Ref = { refKey = "textLabel", refs = refs }
-			}, {
-				"Hello"
-			}),
-		})
-	)
+    gui.frame({ Styles = "bg-red-900 text-scaled" }, {
+      gui.textLabel({
+        Styles = "bg-red-300 text-scaled",
+        Ref = { refKey = "textLabel", refs = refs }
+      }, {
+        "Hello"
+      }),
+    })
+  )
 
-	local updateTextLabel = coroutine.create(function ()
-		while task.wait(1) do
-			if not refs.textLabel then
-				continue
-			end
+  local updateTextLabel = coroutine.create(function ()
+    while task.wait(1) do
+      if not refs.textLabel then
+        continue
+      end
 
-			refs.textLabel.Text = refs.textLabel.Text .. " Test"
-		end
-	end)
+      refs.textLabel.Text = refs.textLabel.Text .. " Test"
+    end
+  end)
 
-	coroutine.resume(updateTextLabel)
+  coroutine.resume(updateTextLabel)
 
-	return newFrame
+  return newFrame
 end
 
 local screenGui = gui.screenGui({}, {
-	frame()
+  frame()
 })
 
 screenGui.Parent = playerGui
